679c749fa76cbf9013d78174ed371d368024713e
kbarker223 <kaiandreasbarker@gmail.com>
2025-12-06 16:21:26 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             title="Message Decryption using MCMC and Simulated Annealing"
             description="Using a Metropolis-Hastings message decryption algorithm, we researched and documented the algorithms functionality and underlying mechanics, and optimized the decoder using simulated annealing. Borrowing simulated annealing from the Traveling Salesman Problem, a similar optimization problem, we updated the Metropolis-Hastings algorithm and discussed its impact on message decryption."
             tech={['Python', 'Latex', 'Simulated Annealing', 'Markov Chain Monte Carlo']}
-            link="/160B_Final_Project.pdf"
+            link="MCMC_and_Simulated_Annealing.pdf"
             linkLabel="View PDF"
             github="https://github.com/LucasC212/Message-Decryption-and-TSP-with-Simulated-Annealing"
           />
